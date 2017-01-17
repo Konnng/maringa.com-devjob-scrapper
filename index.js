a1271b10ba3e246e9779a24c7157d2e5405476d8
Konnng <falecom@konnng.com>
2017-01-17 18:10:57 -0200

    New feature: post jobs openings on devparana's slack group, in #vagas

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ const cheerio = require('cheerio');
 const iconv = require('iconv-lite');
 const sleep = require('system-sleep');
 const moment = require('moment');
+const Slack = require('slack-node');
 // /REQUIRES --------------------------------------------------------------------------------------
 
-const db_file = path.join(__dirname, 'data/db.json');
-if (!fs.existsSync(path.dirname(db_file)) && !fs.mkdirsSync(path.dirname(db_file))) {
+const dbFile = path.join(__dirname, 'data/db.json');
+if (!fs.existsSync(path.dirname(dbFile)) && !fs.mkdirsSync(path.dirname(dbFile))) {
   throw new Error('Error creating data dir.');
 }
-const db = low(db_file, { storage: lowDbFileAsync, writeOnChange: true });
+const db = low(dbFile, { storage: lowDbFileAsync, writeOnChange: true });
 
 const scrapperUrlBase = 'http://www.maringa.com/empregos/index.php';
 const keywords = [
@@ -58,6 +59,7 @@ keywords.forEach((item, index) => {
       let body = iconv.decode(Buffer.concat(bodyChunks), 'latin1').toString();
       let $ = cheerio.load(body);
       let jobs = $('#listaVagas tbody tr').length > 0 ? $('#listaVagas tbody tr') : $('#listaVagas tr').slice(1);
+      let keyword = item
 
       if (jobs.length === 0) {
         return false;
@@ -72,6 +74,7 @@ keywords.forEach((item, index) => {
         let date = job.find('td').eq(2).text().trim();
         let link = job.find('td').eq(0).find('a').attr('href');
         let isFilled = job.find('td').eq(0).find('b').length > 0;
+        let date_processed = Date.now()
 
         if (title.length === 0) {
           return;
@@ -101,7 +104,7 @@ keywords.forEach((item, index) => {
           return;
         }
 
-        row = { id, title, company, date, is_filled: isFilled, url: `http://www.maringa.com/empregos/vaga/${id}/` };
+        row = { id, title, company, date, date_processed, is_filled: isFilled, keyword, bot_processed: false, url: `http://www.maringa.com/empregos/vaga/${id}/` };
 
         db.get('jobs').push(row).value();
       });
@@ -116,6 +119,8 @@ keywords.forEach((item, index) => {
 });
 
 Q.when(deferred.promise).then(() => {
+  let cuttingTime = 1484186400245
+
   console.log('generating html...');
 
   try {
@@ -132,8 +137,45 @@ Q.when(deferred.promise).then(() => {
     }
 
     fs.writeFileSync(outputFile, result);
+//  INTEGRATION WITH DEVPARANA SLACK --------------------------------------------------------------------------------------------
+    console.log('Done generating HTML file: ', outputFile);
+    console.log('Cheking for new job entries to send to Slack...')
+    let botJobs = db.get('jobs').filter({ bot_processed: false }).filter({ is_filled: false }).filter(row => row.date_processed >= cuttingTime).sortBy('date').reverse().value()
+    sleep(1000)
+    if (botJobs.length) {
+      console.log('Found ' + botJobs.length +  ' entries to be posted on slack.');
 
-    console.log('Done ', outputFile);
+      let slack = new Slack();
+      slack.setWebhook('https://hooks.slack.com/services/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');
+
+      botJobs.forEach((item, index) => {
+        console.log('Processing item ' + (index + 1) + '...');
+        slack.webhook({
+          attachments: [
+            {
+              title: item.title + ' / ' + item.company,
+              title_link: item.url,
+              text: 'Vaga: ' + item.title + '\nEmpresa: ' + item.company + '\nData: ' + moment(item.date).format('DD/MM/YYYY'),
+              color: '#7CD197'
+            }
+          ],
+          text: 'Vaga de trabalho encontrada. Confira! \n\n' + item.url,
+        }, function(err, response) {
+          if (err) {
+            console.error(err);
+            console.error('-'.repeat(100));
+            process.exit(1);
+          }
+          if (response.statusCode === 200) {
+            db.get('jobs').find({ id: item.id }).assign({ bot_processed: true }).value();
+          }
+        });
+        sleep(1000);
+      });
+    } else {
+      console.log('No new job opening found to send to slack.')
+    }
+//  /INTEGRATION WITH DEVPARANA SLACK -------------------------------------------------------------------------------------------
   } catch (err) {
     console.error(err);
   }
@@ -141,7 +183,3 @@ Q.when(deferred.promise).then(() => {
   console.error(err);
   process.exit(1);
 });
-
-// process.stdin.once('data', () => {
-//   process.stdin.unref();
-// });
