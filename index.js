4c4c5efdf4df45fa658d955b0b2088464c4f8cf3
Konnng <falecom@konnng.com>
2018-01-10 23:53:20 -0200

    scrapper fixes for the new layout on maringa.com

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const tpl = require('ejs');
 const low = require('lowdb');
 const lowDbFileAsync = require('lowdb/lib/file-async');
-const http = require('http');
+const request = require('request-promise');
 const Q = require('q');
 const fs = require('fs-extra');
 const path = require('path');
@@ -13,20 +13,21 @@ const moment = require('moment');
 const Slack = require('slack-node');
 // /REQUIRES --------------------------------------------------------------------------------------
 
+const SLACK_WEBHOOK = process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA || '';
 const dbFile = path.join(__dirname, 'data/db.json');
 if (!fs.existsSync(path.dirname(dbFile)) && !fs.mkdirsSync(path.dirname(dbFile))) {
   throw new Error('Error creating data dir.');
 }
 const db = low(dbFile, { storage: lowDbFileAsync, writeOnChange: true });
 
-const scrapperUrlBase = 'http://www.maringa.com/empregos/index.php';
+const scrapperUrlBase = 'http://empregos.maringa.com/';
 const keywords = [
   'desenvolvedor',
   'programador',
-  'web',
-  'php',
-  'frontend',
-  'front-end',
+  // 'web',
+  // 'php',
+  // 'frontend',
+  // 'front-end',
 ];
 const blacklist = [
   'torno',
@@ -34,86 +35,110 @@ const blacklist = [
   'ppcp',
   /manuten[\xC7\xE7][\xC3\xE3]o/img,
 ];
+
 const deferred = Q.defer();
+const deferredProcessing = Q.defer();
 const deferredSlack = Q.defer();
 
 db.defaults({ jobs: [], settings: {} }).value();
 
 let keywordsProcessed = 0;
+let websiteToken = '';
+let websiteCookies = [];
 
-keywords.forEach((item, index) => {
-  let url = `${scrapperUrlBase}?busca=${encodeURIComponent(item)}&area=&buscar=Buscar`;
-  let foundJobs = []
+request({ uri: scrapperUrlBase, transform: (body , response) => { return { headers: response.headers, statusCode: response.statusCode, body } } }).then((response) => {
+  if (response.statusCode !== 200) {
+    deferred.reject(new Error('Erro ao obter token e cookies'));
+    return false;
+  }
 
+  const $ = cheerio.load(response.body);
+
+  (response.headers['set-cookie'] || []).forEach((cookie) => {
+    websiteCookies.push(new request.cookie(cookie))
+  });
+
+  websiteToken = $('input[name="_token"]').val();
+
+  if (websiteToken && websiteCookies.length) {
+    deferred.resolve();
+  } else {
+    deferred.reject(new Error('Token de busca inválido.'));
+  }
+}).catch((err) => {
+  console.log(err);
+});
+
+Q.when(deferred.promise).then(() => {
   sleep(1000);
 
-  _log('Processing: ', item);
+  const cookieJar = request.jar();
+  websiteCookies.forEach((cookie) => {
+    cookieJar.setCookie(cookie, scrapperUrlBase);
+  })
 
-  http.get(url, (response) => {
-    let bodyChunks = [];
+  keywords.forEach((keyword, index) => {
+    const foundJobs = [];
 
-    response.on('error', (err) => {
-      _log(err);
-      _log('-'.repeat(100));
-    });
-    response.on('data', (data) => {
-      bodyChunks.push(data);
-    });
-    response.on('end', () => {
-      let body = iconv.decode(Buffer.concat(bodyChunks), 'latin1').toString();
-      let $ = cheerio.load(body);
-      let jobs = $('#listaVagas tbody tr').length > 0 ? $('#listaVagas tbody tr') : $('#listaVagas tr').slice(1);
-      let keyword = item
-
-      if (jobs.length === 0) {
-        return false;
+    request.post(
+      scrapperUrlBase,
+      {
+        jar: cookieJar,
+        form: {
+          _token: websiteToken,
+          text: keyword,
+          estado: 'PR',
+          cidade: 'Maringá',
+          area: '',
+          faixa_salarial: '',
+        },
       }
+    ).then((response) => {
+      const $ = cheerio.load(response);
 
-      jobs.each((i, job) => {
+      $('.table-vagas').find('.clickable-row ').each((i, job) => {
         job = $(job);
 
-        let row = null;
-        let title = job.find('td').eq(0).contents().eq(0).text().trim();
-        let company = job.find('td').eq(1).text().trim();
-        let date = job.find('td').eq(2).text().trim();
-        let link = job.find('td').eq(0).find('a').attr('href');
-        let isFilled = job.find('td').eq(0).find('b').length > 0;
-        let date_processed = Date.now()
+        const title = job.find('td').eq(1).find('p').eq(0).text().trim();
+        const company = job.find('td').eq(1).find('p').eq(1).text().trim().replace(/empresa: /i, '');
+        const link = job.data('href')
 
-        if (title.length === 0) {
-          return;
-        }
+        // TODO: checar se existe flag de vaga preenchida no site
+        const isFilled = false;
+        const date_processed = Date.now();
+        const id = (link.match(/vaga-emprego\/(\d+)\//) || []).pop();
 
-        let id = Number(link.match(/verVaga\((\d*)/i)[1] || 0);
         if (isNaN(id)) {
           return;
         }
+
+        let row = null;
+        let date = job.find('td').eq(2).find('p').eq(0).text().trim();
 
         if (date.toLowerCase() === 'hoje') {
           date = new Date();
         } else {
           date = new Date(date.toString().split('/').reverse().join('-'));
         }
-
         date.setHours(0, 0, 0, 0);
         date = date.getTime();
 
         row = db.get('jobs').find({ id }).value();
-
         if (row) {
-          if (!row.is_filled && isFilled) {
-            db.get('jobs').find({ id }).assign({ is_filed: isFilled }).value();
-          }
+          // TODO: checar se existe flag de vaga preenchida no site
+          // if (!row.is_filled && isFilled) {
+          //   db.get('jobs').find({ id }).assign({ is_filed: isFilled }).value();
+          // }
 
           return;
         }
 
-        foundJobs.push({ id, title, company, date, date_processed, is_filled: isFilled, keyword, bot_processed: false, url: `http://www.maringa.com/empregos/vaga/${id}/` });
+        foundJobs.push({ id, title, company, date, date_processed, is_filled: isFilled, keyword, bot_processed: false, url: link });
       });
 
       foundJobs.filter(job => {
         return blacklist.filter(function (word) {
-          let regex = word.constructor !== RegExp ? new RegExp(`\\b${word}\\b`, 'igm') : word
+          const regex = word.constructor !== RegExp ? new RegExp(`\\b${word}\\b`, 'igm') : word
 
           return regex.test(job.title);
         }).length === 0;
@@ -124,13 +149,17 @@ keywords.forEach((item, index) => {
       keywordsProcessed = index + 1;
 
       if (keywordsProcessed === keywords.length) {
-        setTimeout(function () { deferred.resolve(); }, 1000);
+        setTimeout(function () { deferredProcessing.resolve(); }, 1000);
       }
-    });
+    }).catch((err) => {
+      console.log(err);
+    })
   });
-});
+}, (err) => {
 
-Q.when(deferred.promise).then(() => {
+})
+
+Q.when(deferredProcessing.promise).then(() => {
   let cuttingTime = 1484186400245
 
   _log('generating html...');
@@ -155,11 +184,11 @@ Q.when(deferred.promise).then(() => {
     let botJobs = db.get('jobs').filter({ bot_processed: false }).filter({ is_filled: false }).filter(row => row.date_processed >= cuttingTime).sortBy('date').reverse().value()
     sleep(1000)
 
-    if (process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA && botJobs.length) {
+    if (SLACK_WEBHOOK && botJobs.length) {
       _log('Found ' + botJobs.length + ' entries to be posted on slack.');
 
       let slack = new Slack();
-      slack.setWebhook(process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA);
+      slack.setWebhook(SLACK_WEBHOOK);
 
       botJobs.forEach((item, index) => {
         _log('Processing item ' + (index + 1) + ': ', item.title + ' / ' + item.company);
@@ -193,8 +222,8 @@ Q.when(deferred.promise).then(() => {
         });
         sleep(300);
       });
-    } else if (!process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA) {
-      deferredSlack.reject(new Error('ERROR: Enviroment variable "$LABS_SLACK_WEBHOOK_URL_DEVPARANA" is not defined. Aborting slack...'));
+    } else if (!SLACK_WEBHOOK) {
+      deferredSlack.reject(new Error('ERROR: Enviroment variable "SLACK_WEBHOOK" is not defined. Aborting slack...'));
     } else {
       deferredSlack.resolve('No new job opening found to send to slack.');
     }
