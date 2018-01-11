9102d36a0e6e6dbfb44ddd378190af686431da70
Konnng <falecom@konnng.com>
2018-01-11 12:48:09 -0200

    scrapper fixes, blacklist filter improviments

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const Q = require('q');
 const fs = require('fs-extra');
 const path = require('path');
 const cheerio = require('cheerio');
-const iconv = require('iconv-lite');
 const sleep = require('system-sleep');
 const moment = require('moment');
 const Slack = require('slack-node');
+const Regex = require('xregexp');
 // /REQUIRES --------------------------------------------------------------------------------------
 
 const SLACK_WEBHOOK = process.env.LABS_SLACK_WEBHOOK_URL_DEVPARANA || '';
@@ -24,16 +24,22 @@ const scrapperUrlBase = 'http://empregos.maringa.com/';
 const keywords = [
   'desenvolvedor',
   'programador',
-  // 'web',
-  // 'php',
-  // 'frontend',
-  // 'front-end',
+  'web',
+  'php',
+  'frontend',
+  'front-end',
 ];
 const blacklist = [
   'torno',
   'cnc',
   'ppcp',
-  /manuten[\xC7\xE7][\xC3\xE3]o/img,
+  'usinagem',
+  /vendedor/ig,
+  /servi[\xE7\xC7]os?/ig,
+  /ve[\xCD\xED]culos?/ig,
+  /manuten[\xC7\xE7][\xC3\xE3]o/ig,
+  /neg[\xF3\xD3]cios?/ig,
+  // NOTE: if you decide to use regular expression, don't forget the "ig" flags
 ];
 
 const deferred = Q.defer();
@@ -46,7 +52,14 @@ let keywordsProcessed = 0;
 let websiteToken = '';
 let websiteCookies = [];
 
-request({ uri: scrapperUrlBase, transform: (body , response) => { return { headers: response.headers, statusCode: response.statusCode, body } } }).then((response) => {
+request({
+  uri: scrapperUrlBase,
+  transform: (body, response) => {
+    const res = { headers: response.headers, statusCode: response.statusCode, body };
+
+    return res;
+  },
+}).then((response) => {
   if (response.statusCode !== 200) {
     deferred.reject(new Error('Erro ao obter token e cookies'));
     return false;
@@ -54,31 +67,35 @@ request({ uri: scrapperUrlBase, transform: (body , response) => { return { heade
 
   const $ = cheerio.load(response.body);
 
-  (response.headers['set-cookie'] || []).forEach((cookie) => {
-    websiteCookies.push(new request.cookie(cookie))
+  websiteCookies = (response.headers['set-cookie'] || []).map((cookie) => {
+    const ck = new request.cookie(cookie);
+
+    return ck;
   });
 
   websiteToken = $('input[name="_token"]').val();
 
-  if (websiteToken && websiteCookies.length) {
-    deferred.resolve();
-  } else {
-    deferred.reject(new Error('Token de busca inválido.'));
-  }
+  return websiteToken && websiteCookies.length ? deferred.resolve() : deferred.reject(new Error('Token de busca inválido.'));
 }).catch((err) => {
-  console.log(err);
+  _log('ERROR');
+  _log(err);
+  _log('-'.repeat(100));
+  process.exit(1);
 });
 
 Q.when(deferred.promise).then(() => {
   sleep(1000);
 
+
   const cookieJar = request.jar();
   websiteCookies.forEach((cookie) => {
     cookieJar.setCookie(cookie, scrapperUrlBase);
-  })
+  });
 
   keywords.forEach((keyword, index) => {
-    const foundJobs = [];
+    _log(`Looking for jobs with word "${keyword}`);
+
+    let foundJobs = [];
 
     request.post(
       scrapperUrlBase,
@@ -97,15 +114,15 @@ Q.when(deferred.promise).then(() => {
       const $ = cheerio.load(response);
 
       $('.table-vagas').find('.clickable-row ').each((i, job) => {
-        job = $(job);
+        const $job = $(job);
 
-        const title = job.find('td').eq(1).find('p').eq(0).text().trim();
-        const company = job.find('td').eq(1).find('p').eq(1).text().trim().replace(/empresa: /i, '');
-        const link = job.data('href')
+        const title = $job.find('td').eq(1).find('p').eq(0).find('strong').text().trim().replace(/[\r\n]/g, '');
+        const company = $job.find('td').eq(1).find('p').eq(1).text().trim().replace(/empresa: /i, '');
+        const link = $job.data('href');
 
         // TODO: checar se existe flag de vaga preenchida no site
-        const isFilled = false;
-        const date_processed = Date.now();
+        const isFilled = $job.find('td').eq(1).find('p').eq(0).find('.badge-success').length > 0;
+        const dateProcessed = Date.now();
         const id = (link.match(/vaga-emprego\/(\d+)\//) || []).pop();
 
         if (isNaN(id)) {
@@ -113,7 +130,7 @@ Q.when(deferred.promise).then(() => {
         }
 
         let row = null;
-        let date = job.find('td').eq(2).find('p').eq(0).text().trim();
+        let date = $job.find('td').eq(2).find('p').eq(0).text().trim();
 
         if (date.toLowerCase() === 'hoje') {
           date = new Date();
@@ -125,85 +142,110 @@ Q.when(deferred.promise).then(() => {
 
         row = db.get('jobs').find({ id }).value();
         if (row) {
-          // TODO: checar se existe flag de vaga preenchida no site
-          // if (!row.is_filled && isFilled) {
-          //   db.get('jobs').find({ id }).assign({ is_filed: isFilled }).value();
-          // }
+          if (!row.is_filled && isFilled) {
+            db.get('jobs').find({ id }).assign({ is_filed: isFilled }).value();
+          }
 
           return;
         }
 
-        foundJobs.push({ id, title, company, date, date_processed, is_filled: isFilled, keyword, bot_processed: false, url: link });
+        foundJobs.push({
+          id,
+          title,
+          company,
+          date,
+          date_processed: dateProcessed,
+          is_filled: isFilled,
+          keyword,
+          bot_processed: false,
+          url: link,
+        });
       });
 
-      foundJobs.filter(job => {
-        return blacklist.filter(function (word) {
-          const regex = word.constructor !== RegExp ? new RegExp(`\\b${word}\\b`, 'igm') : word
+      foundJobs = foundJobs.filter((job) => {
+        const test = blacklist.filter((word) => {
+          const regex = word.constructor !== RegExp ? new RegExp(`\\b${word}\\b`, 'igm') : word;
 
-          return regex.test(job.title);
-        }).length === 0;
-      }).forEach(job => {
+          return Regex(regex).test(job.title);
+        });
+
+        return test.length === 0;
+      });
+
+      _log(`Found ${foundJobs.length} job opportunities...`);
+      _log('-'.repeat(100));
+
+      foundJobs.forEach((job) => {
         db.get('jobs').push(job).value();
       });
 
       keywordsProcessed = index + 1;
 
       if (keywordsProcessed === keywords.length) {
-        setTimeout(function () { deferredProcessing.resolve(); }, 1000);
+        setTimeout(() => { deferredProcessing.resolve(); }, 1000);
       }
     }).catch((err) => {
-      console.log(err);
+      _log('ERROR');
+      _log(err);
+      _log('-'.repeat(100));
     })
   });
 }, (err) => {
-
-})
+  _log('ERROR');
+  _log(err);
+  _log('-'.repeat(100));
+});
 
 Q.when(deferredProcessing.promise).then(() => {
-  let cuttingTime = 1484186400245
+  let cuttingTime = 1484186400245;
 
   _log('generating html...');
 
   try {
     db.set('settings.updated_at', Date.now()).value();
 
-    let outputFile = path.join(__dirname, 'out/jobs.html');
-    let html = fs.readFileSync(path.join(__dirname, 'tpl/jobs.ejs'), 'utf8');
+    const outputFile = path.join(__dirname, 'out/jobs.html');
+    const html = fs.readFileSync(path.join(__dirname, 'tpl/jobs.ejs'), 'utf8');
 
-    let jobs = db.get('jobs').filter({ is_filled: false }).sortBy('date').reverse().value() || [];
-    let result = tpl.render(html, { jobs, moment, updated_at: db.get('settings.updated_at').value() || Date.now() });
+    const jobs = db.get('jobs').filter({ is_filled: false }).sortBy('date').reverse().value() || [];
+    const result = tpl.render(html, { jobs, moment, updated_at: db.get('settings.updated_at').value() || Date.now() });
 
     if (!fs.existsSync(path.dirname(outputFile)) && !fs.mkdirsSync(path.dirname(outputFile))) {
       throw new Error('Error creating output directory.');
     }
 
     fs.writeFileSync(outputFile, result);
+
 //  INTEGRATION WITH DEVPARANA SLACK --------------------------------------------------------------------------------------------
     _log('Done generating HTML file: ', outputFile);
     _log('Cheking for new job entries to send to Slack...')
-    let botJobs = db.get('jobs').filter({ bot_processed: false }).filter({ is_filled: false }).filter(row => row.date_processed >= cuttingTime).sortBy('date').reverse().value()
-    sleep(1000)
+
+    const botJobs = db.get('jobs').filter({ bot_processed: false })
+      .filter({ is_filled: false }).filter(row => row.date_processed >= cuttingTime)
+      .sortBy('date').reverse().value();
+
+    sleep(1000);
 
     if (SLACK_WEBHOOK && botJobs.length) {
-      _log('Found ' + botJobs.length + ' entries to be posted on slack.');
+      _log(`Found ${botJobs.length} entries to be posted on slack.`);
 
       let slack = new Slack();
       slack.setWebhook(SLACK_WEBHOOK);
 
       botJobs.forEach((item, index) => {
-        _log('Processing item ' + (index + 1) + ': ', item.title + ' / ' + item.company);
+        _log(`Processing item ${(index + 1)}:`, `${item.title} / ${item.company}`);
 
         slack.webhook({
           attachments: [
             {
-              title: item.title + ' / ' + item.company,
+              title: `${item.title} / ${item.company}`,
               title_link: item.url,
-              text: 'Vaga: ' + item.title + '\nEmpresa: ' + item.company + '\nData: ' + moment(item.date).format('DD/MM/YYYY'),
-              color: '#7CD197'
-            }
+              text: `Vaga:  ${item.title}\nEmpresa: ${item.company}\nData${moment(item.date).format('DD/MM/YYYY')}`,
+              color: '#7CD197',
+            },
           ],
-          text: 'Vaga de trabalho encontrada. Confira! \n\n' + item.url,
-        }, function(err, response) {
+          text: `Vaga de trabalho encontrada. Confira!\n\n${item.url}`,
+        }, (err, response) => {
           if (err) {
             _log('ERROR: ', err);
             _log('ERROR: ', '-'.repeat(100));
@@ -228,10 +270,10 @@ Q.when(deferredProcessing.promise).then(() => {
       deferredSlack.resolve('No new job opening found to send to slack.');
     }
 //  /INTEGRATION WITH DEVPARANA SLACK -------------------------------------------------------------------------------------------
-    Q.when(deferredSlack.promise).then(msg => {
+    Q.when(deferredSlack.promise).then((msg) => {
       _log(msg);
       _log('-'.repeat(100));
-    }, err => {
+    }, (err) => {
       _log('ERROR');
       _log(err);
       _log('-'.repeat(100));
@@ -247,6 +289,6 @@ Q.when(deferredProcessing.promise).then(() => {
   process.exit(1);
 });
 
-function _log() {
-  console.log.apply(console, [].concat([`[${moment().format('DD/MM/YYYY HH:mm:ss')}] =>`], Array.from(arguments) || []))
+function _log(...args) {
+  console.log(...[].concat([`[${moment().format('DD/MM/YYYY HH:mm:ss')}] =>`], Array.from(args) || []));
 }
